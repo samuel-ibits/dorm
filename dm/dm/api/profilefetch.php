5252028421c12e1af2ec7974409c9d0c1502cc14
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-05-20 01:37:35 +0100

    api
    profile api

--- dm/dm/api/profilefetch.php
+++ dm/dm/api/profilefetch.php
@@ -45,16 +45,40 @@ $favfood=$row["favfood"];
 $ihate=$row["ihate"];
 $icherish=$row["icherish"];
 	
-  
- }
-  }response($order_id, $amount, $response_code,$response_desc);
-  mysqli_close($con);
-  }else{
-      response(NULL, NULL, 200,"No Record Found");
-      }
+response($name,$username,$phone,$sta,$mcred,$course,$school,$email,$descyour,$year,$pocketid,$howsch,$descou,$dessch,$dob,$bescou,$besstudtm,$rescrush,$irep,$enjdoing,$favfood,$ihate,$icherish,$order_id, $amount, $response_code,$response_desc); 
+
 }else{
-  response(NULL, NULL, 400,"Invalid Request");
+  close(200,"No Record Found");
+     }
+}else{
+  close(400,"Invalid Request");
+      }
+  
+  
+  
+  mysqli_close($con);
+  
+
+
+
+  function response($order_id,$amount,$response_code,$response_desc){
+    $response['order_id'] = $order_id;
+    $response['amount'] = $amount;
+    $response['response_code'] = $response_code;
+    $response['response_desc'] = $response_desc;
+    
+    $json_response = json_encode($response);
+    echo $json_response;
   }
 
+  function close($response_code,$response_desc){
+    
+    $response['response_code'] = $response_code;
+    $response['response_desc'] = $response_desc;
+    
+    $json_response = json_encode($response);
+    echo $json_response;
+  }
 
+}
   ?>

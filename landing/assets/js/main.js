9f8157574f10f00dafe524da2eab197e97ce39fe
Malik Godwin <malikgodwin2019@gmail.com>
2022-05-30 23:31:19 +0100

    minor update
    minor fix on the the nav bar

--- landing/assets/js/main.js
+++ landing/assets/js/main.js
@@ -46,7 +46,7 @@
   let selectHeader = select("#header");
   if (selectHeader) {
     const headerScrolled = () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 80) {
         selectHeader.classList.add("header-scrolled");
       } else {
         selectHeader.classList.remove("header-scrolled");

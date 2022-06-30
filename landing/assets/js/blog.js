750283e79bde78530dbdef55812954b46366d11c
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-06-30 15:46:09 +0100

    Update blog.js

--- landing/assets/js/blog.js
+++ landing/assets/js/blog.js
@@ -25,13 +25,12 @@ window.addEventListener("load", function () {
     // and getting the response back
     const response = await fetch(api_url, {
       method: "GET", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
+      mode: "no-cors", // no-cors, *cors, same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
       headers: {
         "Content-Type": "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
+      }
     });
 
     // Parsing it to JSON format

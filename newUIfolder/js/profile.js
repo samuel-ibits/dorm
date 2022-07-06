a8efc0172399ea1535c0632ab3e51fbe9350f656
Joe-EtubiGreatattai <98915249+Joe-EtubiGreatattai@users.noreply.github.com>
2022-07-06 23:53:14 +0100

    Add files via upload

---
+++ newUIfolder/js/profile.js
@@ -0,0 +1,301 @@
+function subname(){
+    let name = document.getElementById('name-text');
+    let nameInput = document.getElementById('Name-input');
+
+    let username = document.getElementById('Username-text');
+    let usernameInput = document.getElementById('Username-input');
+    
+
+    let Phone = document.getElementById('Phone-text');
+    let PhoneInput = document.getElementById('Phone-input');
+    
+
+    let Message = document.getElementById('Message-text');
+    let MessageInput = document.getElementById('Message-input');
+    
+    let Course = document.getElementById('Course-text');
+    let CourseInput = document.getElementById('Course-input');
+    
+
+    let Year = document.getElementById('Year-text');
+    let YearInput = document.getElementById('Year-input');
+    
+
+    let School = document.getElementById('School-text');
+    let SchoolInput = document.getElementById('School-input');
+    
+
+    let Bio = document.getElementById('Bio-text');
+    let BioInput = document.getElementById('Bio-input');
+    
+
+    let Date = document.getElementById('Date-text');
+    let DateInput = document.getElementById('Date-input');
+    
+
+    let state = document.getElementById('state-text');
+    let stateinput = document.getElementById('state-input');
+
+    
+    let location = document.getElementById('location-text');
+    let locationinput = document.getElementById('location-input');
+
+    
+    let How = document.getElementById('How-text');
+    let HowInput = document.getElementById('How-input');
+    
+
+    let course = document.getElementById('course-text');
+    let courseInput = document.getElementById('course-input');
+    
+
+    let study = document.getElementById('study-text');
+    let studyInput = document.getElementById('study-input');
+    
+
+    let Recent = document.getElementById('Recent-text');
+    let RecentInput = document.getElementById('Recent-input');
+    
+
+    let represent = document.getElementById('represent-text');
+    let representInput = document.getElementById('represent-input');
+    
+
+    let enjoy = document.getElementById('enjoy-text');
+    let enjoyInput = document.getElementById('enjoy-input');
+    
+
+    let favorite = document.getElementById('favorite-text');
+    let favoriteInput = document.getElementById('favorite-input');
+    
+
+    let hate = document.getElementById('hate-text');
+    let hateInput = document.getElementById('hate-input');
+    
+
+    let love = document.getElementById('love-text');
+    let loveInput = document.getElementById('love-input');
+    
+
+    
+           name.style.display = 'inline-block';
+            nameInput.style.display = 'none';
+            
+            username.style.display = 'inline-block';
+            usernameInput.style.display = 'none';
+           
+            Phone.style.display = 'inline-block';
+            PhoneInput.style.display = 'none';
+            
+            Course.style.display = 'inline-block';
+            CourseInput.style.display = 'none';
+            
+            Year.style.display = 'inline-block';
+            YearInput.style.display = 'none';
+           
+            School.style.display = 'inline-block';
+            SchoolInput.style.display = 'none';
+            
+            Bio.style.display = 'inline-block';
+            BioInput.style.display = 'none';
+
+            
+            state.style.display = 'inline-block';
+            stateinput.style.display = 'none';
+           
+            Date.style.display = 'inline-block';
+            DateInput.style.display = 'none';
+            
+            How.style.display = 'inline-block';
+            HowInput.style.display = 'none';
+           
+            course.style.display = 'inline-block';
+            courseInput.style.display = 'none';
+            
+            study.style.display = 'inline-block';
+            studyInput.style.display = 'none';
+           
+            Recent.style.display = 'inline-block';
+            RecentInput.style.display = 'none';
+            
+            represent.style.display = 'inline-block';
+            representInput.style.display = 'none';
+           
+            enjoy.style.display = 'inline-block';
+            enjoyInput.style.display = 'none';
+            
+            favorite.style.display = 'inline-block';
+            favoriteInput.style.display = 'none';
+           
+            hate.style.display = 'inline-block';
+            hateInput.style.display = 'none';
+            
+            love.style.display = 'inline-block';
+            loveInput.style.display = 'none';
+
+            
+            location.style.display = 'inline-block';
+            locationinput.style.display = 'none';
+           
+            name.innerHTML = "NAME: " + nameInput.value;
+            username.innerHTML = "USER NAME: " + usernameInput.value;
+            Phone.innerHTML = "PHONE NUBMER: " + PhoneInput.value;
+            Course.innerHTML = "COURSE: " + CourseInput.value;
+            Year.innerHTML = "NAME: " + YearInput.value;
+            School.innerHTML = "SCHOOL: " + SchoolInput.value;
+            Bio.innerHTML = "BIO: " + BioInput.value;
+            Date.innerHTML = "DATE: " + DateInput.value;
+            How.innerHTML = "HOW HAS SCHOOL BEEN: " + HowInput.value;
+            location.innerHTML = "LOCATION: " + locationinput.value;
+            state.innerHTML = "STATE: " + stateinput.value;
+            course.innerHTML = "BEST COURSE SO FAR: " + courseInput.value;
+            study.innerHTML = "BEST STUDY TIME: " + studyInput.value;
+            Recent.innerHTML = "RECENT CRUSH: " + RecentInput.value;
+            represent.innerHTML = "WHAT I REPRESENT: " + representInput.value;
+            enjoy.innerHTML = "WHAT I ENJOY DOING THE MOST: " + enjoyInput.value;
+            favorite.innerHTML = "MY FAVOURITE FOOD: " + favoriteInput.value;
+            hate.innerHTML = "WHAT I HATE MOST: " + hateInput.value;
+            love.innerHTML = "WHAT I LOVE: " + loveInput.value;
+        
+            }
+
+
+function editname(){
+    let name = document.getElementById('name-text');
+    let nameInput = document.getElementById('Name-input');
+
+    let username = document.getElementById('Username-text');
+    let usernameInput = document.getElementById('Username-input');
+    
+
+    let Phone = document.getElementById('Phone-text');
+    let PhoneInput = document.getElementById('Phone-input');
+    
+
+    let Message = document.getElementById('Message-text');
+    let MessageInput = document.getElementById('Message-input');
+    
+    let Course = document.getElementById('Course-text');
+    let CourseInput = document.getElementById('Course-input');
+    
+    
+    let state = document.getElementById('state-text');
+    let stateinput = document.getElementById('state-input');
+
+    let location = document.getElementById('location-text');
+    let locationinput = document.getElementById('location-input');
+
+
+    let Year = document.getElementById('Year-text');
+    let YearInput = document.getElementById('Year-input');
+    
+
+    let School = document.getElementById('School-text');
+    let SchoolInput = document.getElementById('School-input');
+    
+
+    let Bio = document.getElementById('Bio-text');
+    let BioInput = document.getElementById('Bio-input');
+    
+
+    let Date = document.getElementById('Date-text');
+    let DateInput = document.getElementById('Date-input');
+    
+
+    let How = document.getElementById('How-text');
+    let HowInput = document.getElementById('How-input');
+    
+
+    let course = document.getElementById('course-text');
+    let courseInput = document.getElementById('course-input');
+    
+
+    let study = document.getElementById('study-text');
+    let studyInput = document.getElementById('study-input');
+    
+  
+    let Recent = document.getElementById('Recent-text');
+    let RecentInput = document.getElementById('Recent-input');
+    
+
+    let represent = document.getElementById('represent-text');
+    let representInput = document.getElementById('represent-input');
+    
+
+    let enjoy = document.getElementById('enjoy-text');
+    let enjoyInput = document.getElementById('enjoy-input');
+    
+
+    let favorite = document.getElementById('favorite-text');
+    let favoriteInput = document.getElementById('favorite-input');
+    
+
+    let hate = document.getElementById('hate-text');
+    let hateInput = document.getElementById('hate-input');
+    
+
+    let love = document.getElementById('love-text');
+    let loveInput = document.getElementById('love-input');
+    
+    
+
+
+    
+           nameInput.style.display = 'inline-block';
+            name.style.display = 'none';
+            
+            usernameInput.style.display = 'inline-block';
+            username.style.display = 'none';
+           
+            PhoneInput.style.display = 'inline-block';
+            Phone.style.display = 'none';
+            
+            CourseInput.style.display = 'inline-block';
+            Course.style.display = 'none';
+            
+            YearInput.style.display = 'inline-block';
+            Year.style.display = 'none';
+           
+            SchoolInput.style.display = 'inline-block';
+            School.style.display = 'none';
+
+            
+            stateinput.style.display = 'inline-block';
+            state.style.display = 'none';
+
+            locationinput.style.display = 'inline-block';
+            location.style.display = 'none';
+            
+            BioInput.style.display = 'inline-block';
+            Bio.style.display = 'none';
+           
+            DateInput.style.display = 'inline-block';
+            Date.style.display = 'none';
+            
+            HowInput.style.display = 'inline-block';
+            How.style.display = 'none';
+           
+            courseInput.style.display = 'inline-block';
+            course.style.display = 'none';
+            
+            studyInput.style.display = 'inline-block';
+            study.style.display = 'none';
+           
+            RecentInput.style.display = 'inline-block';
+            Recent.style.display = 'none';
+            
+            representInput.style.display = 'inline-block';
+            represent.style.display = 'none';
+           
+            enjoyInput.style.display = 'inline-block';
+            enjoy.style.display = 'none';
+            
+            favoriteInput.style.display = 'inline-block';
+            favorite.style.display = 'none';
+           
+            hateInput.style.display = 'inline-block';
+            hate.style.display = 'none';
+            
+            loveInput.style.display = 'inline-block';
+            love.style.display = 'none';
+}

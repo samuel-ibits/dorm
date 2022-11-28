3d26ecbf64cea89610bfd36976b0c7935f9b27fa
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-11-28 11:08:14 +0100

    update

--- qr/landing.php
+++ qr/landing.php
@@ -1,22 +1,26 @@
 <?php
-echo '
-<imput type="hidden" id="demo" name="device"/>
-<script>
-let agent = navigator.userAgent;
-document.getElementById("demo").value = agent;
-</script>';
 
-function pagetrack(){
+
+// function pagetrack(){
     include("../connect.php");
-        $device= $_GET['device'];
+
+//     echo '
+// <form method="get"><imput type="hidden" id="demo" name="device"/>
+// <button type="submit" id="submit" name="submit" />
+// </form>
+// <script>
+// let agent = navigator.userAgent;
+
+// </script>';
+        $device= "no info";
            $date= date("Y-m-d h:i:sa");
      $iiin = "INSERT INTO qr (id, device, date, page) VALUES ( '', '$device', '$date', 'landingpage')";
     if ($conn17->query($iiin)==true) {
         echo '<script> window.location.replace("https://www.dorm.com.ng/landing") </script>';
     }else{echo $conn17->error;}
-    }
+    // }
     
-    pagetrack();
+    // pagetrack();
     
     
 
